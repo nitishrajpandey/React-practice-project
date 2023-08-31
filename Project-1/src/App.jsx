46077c86c048b0ef66e4de4dd8a43fc8ccb5b0d1
nitishrajpandey <nitishrajpandey0707@gmail.com>
2023-08-31 23:32:58 +0530

    complete all setup project in vite js

---
+++ Project-1/src/App.jsx
@@ -0,0 +1,8 @@
+
+function App() {
+  return <>
+  <h1 className=" text-purple-500 font-bold absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-5xl capitalize animate-pulse">project working</h1>
+  </>;
+}
+
+export default App;
