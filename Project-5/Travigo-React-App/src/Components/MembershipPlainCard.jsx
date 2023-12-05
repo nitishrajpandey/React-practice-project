917859c211a73553db6242c7634ab018fc7c9c77
nitishrajpandey <nitishrajpandey0707@gmail.com>
2023-12-05 20:26:01 +0530

    add

--- Project-5/Travigo-React-App/src/Components/MembershipPlainCard.jsx
+++ Project-5/Travigo-React-App/src/Components/MembershipPlainCard.jsx
@@ -1,18 +1,22 @@
 // import React from 'react'
 
+import { memberShipPlanDetails } from "../ContextData/travigoData";
+
 function MembershipPlainCard() {
   return (
     <div>
       <div className="flex flex-col items-center my-20">
         <h1 className="ss:text-2xl md:text-3xl lg:text-4xl text-5xl font-bold mb-4">
-          Choose The Plan That Suits You
+          {memberShipPlanDetails.title}
         </h1>
-        <p className="mb-10">
-          Many attractive offers by becomming a premium member
-        </p>
+        <p className="mb-10">{memberShipPlanDetails.subtitle}</p>
         <div className=" flex gap-5 px-10  py-3 border border-[#e5e7eb] rounded-full  ring-slate-100 ring-1 shadow shadow-slate-200">
-          <button className="button-emrald">Monthly</button>
-          <button className="button-light1">Yearly</button>
+          <button className="button-emrald">
+            {memberShipPlanDetails.btn1}
+          </button>
+          <button className="button-light1">
+            {memberShipPlanDetails.btn2}
+          </button>
         </div>
       </div>
 
